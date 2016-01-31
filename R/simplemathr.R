ae0770dcb84d1b7a4b21fb09a5423a6ad483256d
Alan Arnholt <arnholtat@appstate.edu>
2016-01-31 14:01:24 -0500

    change DESCRIPTION file

---
+++ R/simplemathr.R
@@ -0,0 +1,8 @@
+#' @description Miscelaneous functions for testing code in class.
+#' @details Explain all kinds of stuff here.
+#' @docType package
+#' @name simplemathr-package
+#' @title The most incredible editable simplemathr package
+#' @section My Custom Section: This is just me blathering...yo que se
+#' @keywords package
+NULL
